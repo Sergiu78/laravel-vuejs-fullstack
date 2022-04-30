e33cedc40bfc483507d7d54f36ec1caf20a2fbd6
Sergiu78 <turutaserghei@mail.ru>
2022-04-30 17:54:02 +0300

    Categories

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,8 +11,16 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', \App\Http\Controllers\Main\IndexController::class)->name('main.index');
 
-
+Route::group(['prefix' => 'categories', 'as' => 'category.'], function () {
+    Route::get('/', \App\Http\Controllers\Category\IndexController::class)->name('index');
+    Route::get('/create', \App\Http\Controllers\Category\CreateController::class)->name('crate');
+    Route::post('/', \App\Http\Controllers\Category\StoreController::class)->name('store');
+    Route::get('/{category}/edit', \App\Http\Controllers\Category\EditController::class)->name('edit');
+    Route::get('/{category}', \App\Http\Controllers\Category\ShowController::class)->name('show');
+    Route::patch('/{category}', \App\Http\Controllers\Category\UpdateController::class)->name('update');
+    Route::delete('/{category}', \App\Http\Controllers\Category\DeleteController::class)->name('delete');
+});
